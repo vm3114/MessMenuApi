74588e2b4dd079555ab94e45b1d98700abd16a6f
Vaibhav Mishra <vm311436@gmail.com>
2024-10-10 16:07:41 +0530

    chromedriver config

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ def update_menu(day):
 
     chrome_driver_path = "/usr/bin/chromedriver"
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+
+    chrome_options.add_argument("--headless")  # Run headless
+    chrome_options.add_argument("--no-sandbox")  # Disable sandboxing
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+    chrome_options.add_argument("--window-size=1920,1080")
+    
     service = Service(chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
