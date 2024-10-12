e35834b891e4cd983fa026a2aac218ec0f0ea4d9
Vaibhav Mishra <vm311436@gmail.com>
2024-10-12 07:44:34 +0530

    added features

--- app.py
+++ app.py
@@ -1,40 +1,88 @@
 import pytz
 from functions import get_menu, update_menu, add_response_code
 from fastapi import FastAPI
-from datetime import datetime
+from datetime import datetime,timedelta
 
 
 app = FastAPI()
 timezone = pytz.timezone("Asia/Kolkata")
-today = datetime.now(timezone).strftime("%A").lower()
+dtime_today = datetime.now(timezone)
+today = dtime_today.strftime("%A").lower()
+dtime_tmrw = dtime_today + timedelta(days=1)
+tomorrow = dtime_tmrw.strftime("%A").lower()
+
 days_of_week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+err_response = {"response_code":2,"result":{}}  # usually for undefined query
 
+@app.get("/get")   # /get for today, /get?day=monday for monday, /get?day=tomorrow for tomorrow
+def get(day: str = today):
+    if day.lower() == "tomorrow":
+        day = tomorrow
+    
+    elif day.lower() in days_of_week:
+        day = day.lower()
 
-@app.get("/get")
-@app.get("/get/{day}")
-def get_today(day: str = today):
+    else:
+        return err_response
+    
     return get_menu(day)
 
 
-@app.get("/update")
-@app.get("/update/{day}")
-async def update_today(day: str = today):
+@app.get("/update")   # same as get
+async def update(day: str = today):
+    if day.lower() == "tomorrow":
+        day = tomorrow
+    
+    elif day.lower() in days_of_week:
+        day = day.lower()
+
+    else:
+        return err_response
+    
     return update_menu(day)
 
 
 @app.get("/updateall")
-async def updateAll():
+async def updateAll(start: str = "monday", end: str = "sunday"):  # /updateall for weekly menu, /updateall?start=tuesday&end=friday for menu on tue, wed, thu and fri
+    try:   
+        i = days_of_week.index(start.lower())
+        end_index = days_of_week.index(end.lower())
+
+    except:
+        return err_response
+    
     all_menu = {}
-    for day in days_of_week:
-        menu = update_menu(day)
-        all_menu[day] = menu
+    while True:
+        day = days_of_week[i]
+        all_menu[day] = update_menu(day)
+
+        if i == end_index:
+            break
+        else:
+            i += 1
+            i %= 7
+
     return all_menu
 
 
 @app.get("/getall")
-def getAll():
+def getAll(start: str = "monday", end: str = "sunday"):  # same as updateall
+    try:   
+        i = days_of_week.index(start.lower())
+        end_index = days_of_week.index(end.lower())
+
+    except:
+        return err_response
+    
     all_menu = {}
-    for day in days_of_week:
-        menu = get_menu(day)
-        all_menu[day] = menu
+    while True:
+        day = days_of_week[i]
+        all_menu[day] = get_menu(day)
+
+        if i == end_index:
+            break
+        else:
+            i += 1
+            i %= 7
+    
     return all_menu
